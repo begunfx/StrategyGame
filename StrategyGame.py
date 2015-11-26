6e739f46349c08ab65469175ae0700067b3ecf4a
begunfx <begunfx@usa.net>
2015-11-26 14:46:18 -0800

    Added random function

--- StrategyGame.py
+++ StrategyGame.py
@@ -17,8 +17,18 @@
 #Look for the secret plants to the Death Star
 #Find the Princess (Who is locked up on the Death Star
 #Escape before you run our of turns!
+
+#Put all imports here please
+import random
+
 # get choice function
 def getChoice():
   choice = requestString("What is your next destination choice?")
   return choice          
               
+#Randomize int numbers to pick room to hide plans in
+#Also use to determine what room leads to secret room
+#Hard coded range: 2-8
+def randomizeRoom():
+  randomNum = random.randint(2, 8)
+  return (randomNum)
