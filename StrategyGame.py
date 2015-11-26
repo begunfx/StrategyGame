3cddb9c90ea4f7eed7d36e7589c33fe014660060
begunfx <begunfx@usa.net>
2015-11-26 09:04:39 -0800

    Initial add of our main .py file

---
+++ StrategyGame.py
@@ -0,0 +1,21 @@
+#######Team 4 Strategy Game#######
+#      William Gillihan          #
+#      Christopher Dixon         #
+#      Wendy Gray                #
+#      Brian Begun               #
+#                                #
+#      ver 1.0.0                 #
+#      11_26_2015                #
+##################################
+
+#StrategyGame.py
+#Main File
+#Theme: Star Wars
+#
+#Object of the game:
+#Travel from planet to planet
+#Look for the secret plants to the Death Star
+#Find the Princess (Who is locked up on the Death Star
+#Escape before you run our of turns!
+          
+              
